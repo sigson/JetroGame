3ef557ad19c82844c5a6234c9b68e409ba71ff0e
sigson <79512581+sigson@users.noreply.github.com>
2021-02-23 13:42:37 +0200

    Add files via upload

---
+++ Assets/Content/Land/Saver/Scripts/SaveScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveScript : MonoBehaviour {
+
+	public GameObject GO;
+	public bool Activated;
+	public int SaveNum;
+
+	void Start()
+	{
+		if (PlayerPrefs.HasKey ("AtivatedSavesMax")) {
+			if (PlayerPrefs.GetInt("AtivatedSavesMax")>=SaveNum)
+			{
+				Activated = true;
+				this.GetComponent<Animator>().Play("SavedBlockAnimation");
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (Activated == false) {
+			if (coll.tag == "Player") {
+				Activated = true;
+				//PlayerPrefs.SetString("StartedFlag", "Started"); //Started or InSave
+				this.GetComponent<Animator>().Play("SavedBlockAnimation");
+				PlayerPrefs.SetFloat("PosX", GO.transform.position.x);
+				PlayerPrefs.SetFloat("PosY", GO.transform.position.y);
+				PlayerPrefs.SetInt("AtivatedSavesMax", SaveNum);
+			}
+		}
+	}
+}
