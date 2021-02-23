84214206b252d48527b3d1dc083aa11f8912af5a
sigson <79512581+sigson@users.noreply.github.com>
2021-02-23 13:41:34 +0200

    Add files via upload

---
+++ Assets/Content/Land/Drop/Coin/CoinWorker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinWorker : MonoBehaviour {
+
+	public int CoinValue;
+	public bool Activated = false;
+	
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (Activated == false) {
+			if (coll.tag == "Player") {
+				Activated = true;
+				coll.GetComponentInParent<Entity>().AddCoin(CoinValue);
+				this.GetComponent<Animator>().Play("Coin getting");
+
+			}
+		}
+	}
+
+	public void StopAnimation()
+	{
+		this.GetComponent<Animator>().speed = 0f;
+		this.GetComponentInParent<Obj>().gameObject.SetActive(false);
+	}
+}
